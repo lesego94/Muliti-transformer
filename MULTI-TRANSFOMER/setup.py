a485f8a3d3fbb1dbe08429a6b75beb1100a04d4a
Lesego Matojane <lmatojane@gmail.com>
2023-05-13 18:49:45 +0200

    new project

---
+++ MULTI-TRANSFOMER/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use of multi transformer architectures for portfolio optimization',
+    author='Lesego Matojane',
+    license='BSD-3',
+)
